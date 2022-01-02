895b2543a0e17b9cf34df25671bb685a2c8409f8
yeng4011 <89770222+yeng4011@users.noreply.github.com>
2022-01-03 03:12:02 +0800

    Create Class07.java

---
+++ Class07.java
@@ -0,0 +1,22 @@
+package Class07.java;
+
+ import java.io.*;
+ public class Class07
+ {
+ public static void main(String args[]) throws IOException
+ {
+ int count=0;
+ String str;
+ FileReader fr=new FileReader("c:\\java\\donkey.txt");
+ BufferedReader bfr=new BufferedReader(fr);
+
+ while((str=bfr.readLine())!=null)
+ {
+ if(count==1)
+ bfr.skip(14);
+ System.out.println(str);
+ count++;
+ }
+ fr.close();
+ }
+ }
